abaea798996496b4777ce207c9159fb348d63ee5
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-20 20:03:46 +0300

    Puppet: install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,12 @@
 # Installs flask from pip3 using puppet
 
 package{'python3':
-  ensure=> 3.8.10,
+  ensure => installed,
 }
 
 package{'flask':
-  ensure   => '2.1.0',
-  require  => Package['pyhton3'],
-  provider =>'pip3'
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  require  => Package['python3'],
-  provider => 'pip3'
+  ensure          => '2.1.0',
+  require         => Package['python3'],
+  provider        =>'pip3',
+  install_options => ['--break-system-packages']
 }
