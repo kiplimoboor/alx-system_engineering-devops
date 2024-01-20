229f904a9fbf518db1c00ec84cc4c6d6bdc21ce8
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-20 13:58:09 +0300

    Puppet: install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,8 @@
 # Installs Flask version 2.1.0
 
 package{
-  'werkzeug':
-  ensure          => '2.1.1',
-  provider        => pip3,
-  install_options => ['--break-system-packages']
-}
-
-package{
   'flask':
   ensure          =>'2.1.0',
   provider        =>pip3,
-  require         => Package['werkzeug'],
   install_options => ['--break-system-packages'],
 }
