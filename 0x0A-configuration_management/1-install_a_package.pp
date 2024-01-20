08f3b1bbdb32e535353c26bb0ff26a0881d2d866
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-20 20:10:29 +0300

    Puppet: install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,13 +5,13 @@ package{'python3-pip':
 }
 
 package{'flask':
-  ensure          => '2.1.0',
-  require         => Package['python3-pip'],
-  provider        =>'pip3',
+  ensure   => '2.1.0',
+  require  => Package['python3-pip'],
+  provider =>'pip3',
 }
 
 package { 'werkzeug':
-  ensure          => '2.1.1',
-  require         => Package['python3-pip'],
-  provider        => 'pip3',
+  ensure   => '2.1.1',
+  require  => Package['python3-pip'],
+  provider => 'pip3',
 }
