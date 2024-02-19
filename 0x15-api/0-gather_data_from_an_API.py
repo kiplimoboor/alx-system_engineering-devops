9553ae2667045962da377ee9e32666aab6ba0ece
kiplimoboor <kiplimoboor@hotmail.com>
2024-02-19 18:13:51 +0300

    simple api request

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-    A simple module to make API Calls
-    To a mockup API server and return the
-    Responses. Then print them out to standard output
-    Usage: ./0-gather-data_from_an_API <ID>
-    Where <ID> is the employee ID for whom we want to list
-    The tasks
+    Simple API Call from JSONPlaceholder
 """
+
 
 from requests import get
 from sys import argv
 
-user_id = argv[1]
-api_link = 'https://jsonplaceholder.typicode.com'
-r = get(f'{api_link}/users/{user_id}')
-name = r.json().get('name')
-r = get(f'{api_link}/todos/?userId={user_id}&completed=true')
-todos = r.json()
-
-print(f'Employee {name} is done with tasks({len(todos)}/20):')
-for todo in todos:
-    print(f'\t{todo.get("title")}')
+if __name__ == '__main__':
+    user_id = argv[1]
+    api_link = 'https://jsonplaceholder.typicode.com'
+    r = get(f'{api_link}/users/{user_id}')
+    name = r.json().get('name')
+    r = get(f'{api_link}/todos/?userId={user_id}&completed=true')
+    todos = r.json()
+    print(f'Employee {name} is done with tasks({len(todos)}/20):')
+    for todo in todos:
+        print(f'\t{todo.get("title")}')
