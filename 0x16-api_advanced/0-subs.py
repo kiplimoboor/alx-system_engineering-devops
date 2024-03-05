05c4f0552a6a727c82b01d3af6e714d4e590f8b5
kiplimoboor <kiplimoboor@hotmail.com>
2024-03-05 16:41:18 +0300

    subscriber count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,8 @@ def number_of_subscribers(subreddit):
         If the subreddit is nonexistent, return a 0
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers={'User-Agent': 'kiplimoboor'})
+    response = requests.get(
+        url, headers={'User-Agent': 'kiplimoboor'}, allow_redirects=False)
     if response.status_code != 200:
         return 0
     return response.json().get('data').get('subscribers')
