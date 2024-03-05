2ea7fe77070fbe41a037fe08e24310864a14d47f
kiplimoboor <kiplimoboor@hotmail.com>
2024-03-05 16:38:01 +0300

    gitignore

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Gets subscriber count of a subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        This functions takes in a subreddit name and returns the
+        subscriber count.
+        If the subreddit is nonexistent, return a 0
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers={'User-Agent': 'kiplimoboor'})
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
